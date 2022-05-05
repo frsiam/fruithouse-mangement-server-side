8ff5c3b85366fcc8b48d8a20d047d678e5b6216b
frsiam <siam.frs@gmail.com>
2022-05-05 13:02:14 +0600

    create a mongodb user and password is save into my .env file for security purpose

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihinm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://fruitHouseOwner:WOFxQf62HEk2gIY9@cluster0.ihinm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
 app.get('/', (req, res) => {
