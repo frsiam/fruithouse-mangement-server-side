d22b8bd6507f01875f40427480913f83ee2b42be
frsiam <siam.frs@gmail.com>
2022-05-05 15:02:14 +0600

    database connection done

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 4000;
+
+require('dotenv').config()
 
 const app = express();
+const port = process.env.PORT || 4000;
 
+// use midleware
 app.use(cors())
 app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ihinm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://fruitHouseOwner:WOFxQf62HEk2gIY9@cluster0.ihinm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const fruitCollection = client.db("fruitExpress").collection("fruit");
+
+
+        // Data received from UI and send to UI
+        app.get('/fruit', (req, res) => {
+            res.send('from new db')
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
